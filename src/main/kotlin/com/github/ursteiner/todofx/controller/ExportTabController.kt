74dcdc62dae71ab3b93844879a490477c8c8a723
ursteiner <christoph.mueller.zw@googlemail.com>
2025-05-02 07:58:43 +0200

    #19 Added an export explanation

--- src/main/kotlin/com/github/ursteiner/todofx/controller/ExportTabController.kt
+++ src/main/kotlin/com/github/ursteiner/todofx/controller/ExportTabController.kt
@@ -1,10 +1,26 @@
 package com.github.ursteiner.todofx.controller
 
+import com.github.ursteiner.todofx.constants.TranslationKeys
 import com.github.ursteiner.todofx.service.CsvExportServiceImpl
 import com.github.ursteiner.todofx.service.DatabaseServiceImpl
+import com.github.ursteiner.todofx.service.LanguageServiceImpl
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Label
+import java.net.URL
+import java.util.ResourceBundle
 
-class ExportTabController {
+class ExportTabController: Initializable {
+
+    @FXML
+    private lateinit var exportLabel: Label
+
+    private lateinit var languageService : LanguageServiceImpl
+
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+        languageService = LanguageServiceImpl.getInstance(System.getProperty("user.language"))
+        exportLabel.text = languageService.getTranslationForKey(TranslationKeys.EXPORT_IS_STORED_TO_APPLICATION_FOLDER)
+    }
 
     @FXML
     fun onExportTaskButtonClick(){
