e37417948aa37fd1280f77e9bdfc91935db41775
ursteiner <christoph.mueller.zw@googlemail.com>
2025-08-08 16:25:27 +0200

    #46 added a JUnit test for DateUtils

---
+++ src/test/kotlin/com/github/ursteiner/todofx/utils/DateUtilsTest.kt
@@ -0,0 +1,26 @@
+package com.github.ursteiner.todofx.utils
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class DateUtilsTest {
+
+    private lateinit var testCandidate: DateUtils
+
+    @BeforeEach
+    fun setUp(){
+        testCandidate = DateUtils
+    }
+
+    @Test
+    fun testGetYearMonth(){
+        val sdf = SimpleDateFormat("yyyy-MM", Locale.getDefault())
+        val currentDate = sdf.format(Date())
+
+        Assertions.assertEquals(currentDate, testCandidate.getYearMonth(0))
+    }
+}
