3b6ceb89d36b934d42ebdfab9f902cc324ef88a2
ursteiner <christoph.mueller.zw@googlemail.com>
2026-08-26 21:24:04 +0200

    fix module-info

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -1,4 +1,4 @@
-module com.github.ursteiner.todofx {
+open module com.github.ursteiner.todofx {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
@@ -8,9 +8,7 @@ module com.github.ursteiner.todofx {
     requires kotlinx.coroutines.core;
     requires kotlinx.serialization.json;
     requires org.slf4j;
+    requires static org.junit.jupiter.api;
 
-    opens com.github.ursteiner.todofx to javafx.fxml, javafx.controls, kotlinx.serialization.json, org.slf4j, exposed.core, exposed.jdbc;
-    opens com.github.ursteiner.todofx.controller to javafx.fxml, javafx.controls;
-    opens com.github.ursteiner.todofx.model to javafx.base;
     exports com.github.ursteiner.todofx;
 }
